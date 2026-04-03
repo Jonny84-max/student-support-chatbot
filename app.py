833987bfdb1d181a6667cf72dceff740aa1bb582
Jonny84-max <jonnybest0684@gmail.com>
2026-04-03 23:36:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ if days_to_start > 0:
 else:
     countdown = ""   # Neutral if exam has started or passed
     show_days = False
-
-return first_exam_day, last_exam_day, countdown, show_days   # resturn everything
+# resturn everything
+return first_exam_day, last_exam_day, countdown, show_days   
 
 # Generate schedules
 f_start, f_end, f_count, f_days = get_exam_details(3, 1)   # 1st sem: March, 1st week
